97b2a14ee821dd6bebeb9d693ddb0a3bc4239d96
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-07 21:47:29 +0000

    automatically backed up by learn

--- Tic_Tac_Toe.rb
+++ Tic_Tac_Toe.rb
@@ -13,11 +13,11 @@ decision1 = gets.chomp
 def numbers_check
 return (decision1 !=  “1” and decision1 !=  “2” and decision1 !=  “3” and decision1 !=  “4” and decision1 !=  “5” and decision1 !=  “6” and decision1 !=  “7” and decision1 !=  “8” and decision1 !=  “9”)
 end
-if numbers_check
-puts “Unacceptable, user! I will ask you one more time: Where would X like to go?”
+if numbers_check = true
+puts “Unacceptable, user! I will ask you one more time. Where would X like to go?”
 decision1 = gets.chomp
 end
-if numbers_check
+if numbers_check = true
 puts “I’m sick of your crap, user. Game over.”
 end
 decision1 = decision1.to_i
